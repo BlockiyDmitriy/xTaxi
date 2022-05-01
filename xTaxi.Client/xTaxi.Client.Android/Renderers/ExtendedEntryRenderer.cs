38e3dd1ab52e96345861e4d1009ce5c4df909a62
Blockiy_Dmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2022-05-01 17:55:26 +0300

    Add entry renderer

---
+++ xTaxi.Client/xTaxi.Client.Android/Renderers/ExtendedEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using xTaxi.Client.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(ExtendedEntryRenderer))]
+namespace xTaxi.Client.Droid.Renderers
+{
+    public class ExtendedEntryRenderer : EntryRenderer
+    {
+        public ExtendedEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackground(null);
+            }
+        }
+    }
+}
---
+++ xTaxi.Client/xTaxi.Client/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xTaxi.Client.Constants
+{
+    public static class Constants
+    {
+        public const string GoogleMapsApiKey = "AIzaSyC4RHyOfC2__hVFr9BWlIN0Zy5pRj42P8A";
+    }
+}
