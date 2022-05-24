d710c472cef9e066c3fae5e67966db0a5503ec39
Blockiy_Dmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2022-05-24 21:11:49 +0300

    Add new create account page

---
+++ xTaxi.Client/xTaxi.Client/Pages/CreateNewAccountPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using xTaxi.Client.ViewModels;
+
+namespace xTaxi.Client.Pages
+{
+    public partial class CreateNewAccountPage : ContentPage
+    {
+        private readonly CreateNewAccountViewModel _viewModel;
+
+        public CreateNewAccountPage()
+        {
+            InitializeComponent();
+
+            _viewModel = new CreateNewAccountViewModel();
+            BindingContext = _viewModel;
+        }
+    }
+}
---
+++ xTaxi.Client/xTaxi.Client/ViewModels/CreateNewAccountViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using xTaxi.Client.Services.Logger;
+using xTaxi.Client.Utils;
+using xTaxi.Client.Views;
+
+namespace xTaxi.Client.ViewModels
+{
+    public class CreateNewAccountViewModel : INotifyPropertyChanged
+    {
+        private readonly ILogService _logService;
+
+        public ICommand CreateAccountCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public CreateNewAccountViewModel()
+        {
+            _logService = DependencyResolver.Get<ILogService>();
+
+            CreateAccountCommand = new Command(async () => await CreateAccount());
+            CancelCommand = new Command(async () => await Cancel());
+        }
+
+        private Task Cancel()
+        {
+            try
+            {
+                Shell.Current.SendBackButtonPressed();
+                return Task.CompletedTask;
+            }
+            catch (Exception e)
+            {
+                _logService.TrackException(e, MethodBase.GetCurrentMethod()?.Name);
+                return Task.CompletedTask;
+            }
+        }
+
+        private async Task CreateAccount()
+        {
+            try
+            {
+
+                await Shell.Current.GoToAsync($"//{nameof(MapPage)}");
+            }
+            catch (Exception e)
+            {
+                _logService.TrackException(e, MethodBase.GetCurrentMethod()?.Name);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
