fd4c2cd6ca9b2d52be7e3cd5c2e935b589109958
Blockiy_Dmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2022-05-24 21:09:27 +0300

    add error popup

---
+++ xTaxi.Client/xTaxi.Client/Views/ErrorPopup.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+
+namespace xTaxi.Client.Views
+{
+    public partial class ErrorPopup : PopupPage
+    {
+        public string Message { get; set; }
+
+        public ErrorPopup(string message)
+        {
+            InitializeComponent();
+
+            Message = message;
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
